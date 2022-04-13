8a3b28afede1821653c65df3432343433b99eac3
Sabrina <34215580+BwinaRose@users.noreply.github.com>
2022-04-13 17:58:37 -0400

    added budget icons and modal menus

--- devSE/TrackYourBread/budget.js
+++ devSE/TrackYourBread/budget.js
@@ -5,12 +5,57 @@ const monthArr = ["January", "February", "March", "April", "May", "June", "July"
 const month = monthArr[m];
 document.getElementById("date").innerHTML = month + ' ' + y;
 
-// Get the modal
-var modal = document.getElementById('id012');
 
-// When the user clicks anywhere outside of the modal, close it
+
+// Get the modal
+var modal = document.getElementById('imodal');
+var btn = document.getElementById('ibtn');
+var btn2 = document.getElementById('ibtn2');
+var btn3 = document.getElementById('ibtn3');
+var btn4 = document.getElementById('ibtn4');
+var btn5 = document.getElementById('ibtn5');
+var btn6 = document.getElementById('ibtn6');
+var btn7 = document.getElementById('ibtn7');
+var btn8 = document.getElementById('ibtn8');
+var btn9 = document.getElementById('ibtn9');
+var btn10 = document.getElementById('ibtn10');
+var span = document.getElementsByClassName('close')[0];
+
+btn.onclick = function() {
+    modal.style.display = 'block';
+}
+btn2.onclick = function() {
+    modal.style.display = 'block';
+}
+btn3.onclick = function() {
+    modal.style.display = 'block';
+}
+btn4.onclick = function() {
+    modal.style.display = 'block';
+}
+btn5.onclick = function() {
+    modal.style.display = 'block';
+}
+btn6.onclick = function() {
+    modal.style.display = 'block';
+}
+btn7.onclick = function() {
+    modal.style.display = 'block';
+}
+btn8.onclick = function() {
+    modal.style.display = 'block';
+}
+btn9.onclick = function() {
+    modal.style.display = 'block';
+}
+btn10.onclick = function() {
+    modal.style.display = 'block';
+}
+span.onclick = function() {
+    modal.style.display = 'none';
+}
 window.onclick = function(event) {
     if (event.target == modal) {
-        modal.style.display = "none";
+        modal.style.display = 'none';
     }
 }
