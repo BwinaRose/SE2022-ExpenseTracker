90e51600fb08801fdcb8bf99c938a2c4399d0cb8
BwinaRose <sabrinakrueger24@gmail.com>
2022-04-25 15:59:02 -0400

    working on new CSS, but still ugly

---
+++ devSE/TrackYourBread_v2/js/dash.js
@@ -0,0 +1,36 @@
+const balance = document.getElementById('balance');
+
+// const localStorageTransactions = JSON.parse(
+//     localStorage.getItem('transactions')
+// );
+
+// let transactions =
+//     localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
+
+let transactions = JSON.parse(localStorage.getItem('transactions'));
+
+const transactionLocal = transactions
+
+const amounts = transactions.map(transaction => transaction.amount);
+
+const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+console.log(transactions);
+//console.log(transactionLocal)
+
+
+// const iterator = transactions.values()
+// for (let key of iterator) {
+//     console.log(key);
+// }
+
+const iterator = transactions.values()
+for (let key of iterator) {
+    console.log(transactions[key]);
+}
+
+
+
+
+
+// console.log(total);
